78ae1d327f737ee923ff654ba3c4d619e8922fd0
Mustafa Yurdakul <yurdakulmustafa@outlook.com>
2020-09-03 08:53:29 +0300

    Components added.

---
+++ src/app/components/gamecontrol/gamecontrol.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gamecontrol',
+  templateUrl: './gamecontrol.component.html',
+  styleUrls: ['./gamecontrol.component.scss']
+})
+export class GamecontrolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
