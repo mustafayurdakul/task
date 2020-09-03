c25d4e96b2adaa2e97988fc2999bd9c97900cf33
Mustafa Yurdakul <yurdakulmustafa@outlook.com>
2020-09-03 15:08:57 +0300

    App Component Updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
+
+/* Parent */
 export class AppComponent {
-  title = 'task';
+    seconds = 0
+    interval = 0
+
+    oddnumbers = []
+    evennumbers = []
+
+    constructor() { }
+
+    ngOnInit(): void {
+
+    }
+
+    clearTimer() {
+        clearInterval(this.interval);
+    }
+
+    start() {
+        this.counter();
+    }
+
+    stop() {
+        this.clearTimer();
+    }
+
+    private counter() {
+        this.clearTimer()
+        this.interval = window.setInterval(() => {
+            if (this.seconds % 2 == 0) {
+                this.evennumbers.push(this.seconds)
+            } else {
+                this.oddnumbers.push(this.seconds)
+            }
+            this.seconds += 1;
+            console.log(this.seconds)
+            if (this.seconds === 10) {
+                console.log(this.oddnumbers, this.evennumbers)
+            } else {
+                if (this.seconds >= 10) {
+                    this.seconds = 0
+                    this.evennumbers = []
+                    this.oddnumbers = []
+                }
+            }
+        }, 1000);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamecontrolComponent } from './components/gamecontrol/gamecontrol.component';
+import { OddComponent } from './components/odd/odd.component';
+import { EvenComponent } from './components/even/even.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        GamecontrolComponent,
+        OddComponent,
+        EvenComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgbModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
